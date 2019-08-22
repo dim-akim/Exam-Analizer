69e5ecb35e52d09f086d7675d7cfdbb2196aaee6
dim-akim <akimov@1060.ru>
2019-08-22 21:30:26 +0300

    Исправлены мелкие ошибки в классе ResultFile. Класс протестирован на файле "Пример". ПРОБЛЕМА - ключ  None со значениями None в конце словаря

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 """
 Модуль для тестирования
 """
-
+"""
 from openpyxl import load_workbook as lw
 
 
@@ -10,4 +10,16 @@ page = file.active
 
 for i in range (1,page.max_column + 1):
     print (page.cell(row=11, column=i)).value
+"""
 
+from main import *
+
+
+filename = 'Результаты 2019/Пример.xlsx'
+file = ResultFile(filename)
+print(file.name)
+print(file.sheet)
+for i in file.students.keys():
+    print(i)
+    for j in file.students[i]:
+        print(j)
